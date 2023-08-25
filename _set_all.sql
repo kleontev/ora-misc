03a894c4c9e2f642083933d356038545ff3435db
Kirill Leontev <leontiev.k@gmail.com>
2023-08-25 20:38:47 +0700

    set first, then cols

--- _set_all.sql
+++ _set_all.sql
@@ -1,18 +1,5 @@
-clear columns 
-
-col inst_id for 99
-col sid for 99999
-col serial# for 99999
-col schemaname for a20
-col osuser for a10
-col seconds_in_wait for 999,999
-
-col "LINE/COL" for a10
-col "ERROR" for a65 wrap
-
-
-set head on 
 set echo off
+set head on 
 set time on
 set timi on
 set feedback on
@@ -25,3 +12,15 @@ set lines 80
 set long 200000000
 set longchunksize 200000000
 set tab off
+
+clear columns 
+
+col inst_id for 99
+col sid for 99999
+col serial# for 99999
+col schemaname for a20
+col osuser for a10
+col seconds_in_wait for 999,999
+
+col "LINE/COL" for a10
+col "ERROR" for a65 wrap
