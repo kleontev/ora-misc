9a2dd63fbc0a1d924fd48fa890f4935c11bf36ec
Kirill Leontev <leontiev.k@gmail.com>
2023-10-12 00:52:33 +0700

    list db objects

---
+++ ls.sql
@@ -0,0 +1,33 @@
+@_save_buffer 
+@_save_set 
+
+set lines 2000
+
+col object_type for a20
+col object_owner for a15
+col object_name for a30
+col subobject_name for a30
+col status for a20
+
+break on object_type page
+
+select 
+    object_type,
+    owner object_owner,
+    object_name,
+    subobject_name,
+    created,
+    last_ddl_time,
+    status
+from dba_objects 
+where 1 = 1
+    and owner || '.' || object_name like upper('&1.')
+order by 
+    object_Type, 
+    owner,
+    object_name
+fetch first 100 rows only
+/
+
+@_read_buffer
+@_read_set
