54fe7db126886a37ac38f5a850933dfb8b7994ce
Kirill Leontev <leontiev.k@gmail.com>
2023-10-12 00:36:53 +0700

    (sqlplus) in the window title removed some whitespace

--- login.sql
+++ login.sql
@@ -1,10 +1,10 @@
--- most likely I don't want to see what's going on in there 
+-- most likely I don't want to see what's going on in there
 set termout off
 
 
 -- obtain and store session metadata somewher
 def _my_inst_id = ""
-def _my_sid = "" 
+def _my_sid = ""
 def _my_serial = ""
 def _my_prompt = "SQL> "
 
@@ -13,20 +13,20 @@ col sid new_value _my_sid
 col serial# new_value _my_serial
 col my_prompt new_value _my_prompt
 
-select 
-    sys_context('userenv', 'instance') inst_id, 
+select
+    sys_context('userenv', 'instance') inst_id,
     trim(sid) sid,
     trim(serial#) serial#
-from v$session s 
+from v$session s
 where s.sid = sys_context('userenv', 'sid')
 /
 
 -- init sqlplus prompt, if connected
 -- will default to "SQL> " if not
 select
-       '&_connect_identifier, ' 
+       '&_connect_identifier, '
     || '&_user, '
-    || 'sid=&_my_sid., ' 
+    || 'sid=&_my_sid., '
     || 'serial=&_my_serial.> ' my_prompt
 from dual
 /
@@ -36,7 +36,7 @@ undef _my_prompt
 
 
 -- change sqlplus window header in win
-ho title -
+ho title (sqlplus) -
 db=&_connect_identifier.,-
 user=&_user.,-
 inst_id=&_my_inst_id,-
@@ -54,9 +54,9 @@ alter session set plsql_warnings = 'enable:all';
 
 
 -- init common paths for buffers and spools
-def _my_temp_dir = "c:\users\kir-leontev\oracle\tmp"     
+def _my_temp_dir = "c:\users\kir-leontev\oracle\tmp"
 def _my_ed_path = "&_my_temp_dir.\&_my_sid._edit.sql"
-def _my_buffer_path = "&_my_temp_dir.\&_my_sid._buffer.sql" 
+def _my_buffer_path = "&_my_temp_dir.\&_my_sid._buffer.sql"
 def _my_set_path = "&_my_temp_dir.\&_my_sid._set.sql"
 
 
@@ -73,20 +73,20 @@ exec dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'CONSTRA
 exec dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'SEGMENT_ATTRIBUTES', false);
 
 
--- set my default sqlplus parameters 
+-- set my default sqlplus parameters
 set echo off
 set feedback on
-set head on 
+set head on
 set lines 80
 set long 200000000
 set longchunksize 200000000
 set pages 50
 set tab off
-set termout on 
+set termout on
 set time on
 set timi on
-set trimspool on 
-set trimout on 
+set trimspool on
+set trimout on
 set verify off
 
 
