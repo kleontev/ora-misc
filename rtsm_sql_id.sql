249ef9ea241777c3cbd62abbd0d06dfc898ac65b
Kirill Leontev <leontiev.k@gmail.com>
2024-09-08 00:24:50 +0700

    rtsm_sql_id: connect identifier in rtsm_dir

--- rtsm_sql_id.sql
+++ rtsm_sql_id.sql
@@ -6,12 +6,15 @@
 
 set termout off
 
-column d new_value _rtsm_date 
+column d new_value _rtsm_date
 select to_char(sysdate, 'yyyymmdd_hh24miss') as d from dual
 /
 
-def rtsm_path = &_my_temp_dir.\rtsm\&_rtsm_date._&1..html
+def rtsm_dir = &_my_temp_dir.\rtsm\&_connect_identifier.\
 
+ho mkdir &rtsm_dir. 2> nul
+
+def rtsm_path = &rtsm_dir.\&_rtsm_date._&1..html
 
 select
     dbms_sql_monitor.report_sql_monitor(
@@ -27,10 +30,11 @@ from dual
 spool &rtsm_path.
 /
 spool off
-set termout on 
+set termout on
 pro Saved RTSM report to &rtsm_path.
 
 undef rtsm_path
+undef rtsm_dir
 
 @_unset_def_val
 @_read_buffer
